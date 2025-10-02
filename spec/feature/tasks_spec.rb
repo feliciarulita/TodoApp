81e649ec52dfa315c60fd63faccc01db0a49d070
feliciarulita <feliciarulita@gmail.com>
2025-10-02 11:08:45 +0800

    fix rspec test

--- spec/feature/tasks_spec.rb
+++ spec/feature/tasks_spec.rb
@@ -11,16 +11,16 @@ RSpec.describe "Tasks", type: :system do
   context "when creating a task" do
     before do
       visit new_task_path
-      fill_in "Name", with: "Task 1"
-      fill_in "Create time", with: "2025-09-30T15:00"
-      fill_in "End time", with: "2025-10-01T18:00"
-      select "Pending", from: "Status"
-      select "High", from: "Priority"
-      fill_in "Tag", with: "Work"
-      click_button "Create Task"
+      fill_in I18n.t("activerecord.attributes.task.name"), with: "Task 1"
+      fill_in I18n.t("activerecord.attributes.task.create_time"), with: "2025-09-30T15:00"
+      fill_in I18n.t("activerecord.attributes.task.end_time"), with: "2025-10-01T18:00"
+      select I18n.t("activerecord.attributes.task.statuses.pending"), from: "Status"
+      select I18n.t("activerecord.attributes.task.priorities.high"), from: "Priority"
+      fill_in I18n.t("activerecord.attributes.task.tag"), with: "Work"
+      click_button I18n.t("todo.addTask")
     end
 
-    it { is_expected.to have_content("Task was successfully created.") }
+    it { is_expected.to have_content(I18n.t("notice.createSuccess")) }
     it { is_expected.to have_content("Task 1") }
   end
 
@@ -30,7 +30,7 @@ RSpec.describe "Tasks", type: :system do
       visit root_path
     end
 
-    it { is_expected.to have_content("Tasks") }
+    it { is_expected.to have_content(I18n.t("todo.title")) }
     it { is_expected.to have_content("Task 2") }
   end
 
@@ -38,14 +38,14 @@ RSpec.describe "Tasks", type: :system do
     let!(:task) { create(:task, name: "Task 3", status: :pending, priority: :low) }
 
     before do
-      visit edit_task_path(task)
-      fill_in "Name", with: "Updated Task 3"
-      select "In progress", from: "Status"
-      select "High", from: "Priority"
-      click_button "Update Task"
+      visit edit_task_path(I18n.locale, task)
+      fill_in I18n.t("activerecord.attributes.task.name"), with: "Updated Task 3"
+      select I18n.t("activerecord.attributes.task.statuses.in_progress"), from: "Status"
+      select I18n.t("activerecord.attributes.task.priorities.high"), from: "Priority"
+      click_button I18n.t("todo.updateTask")
     end
 
-    it { is_expected.to have_content("Task was successfully updated.") }
+    it { is_expected.to have_content(I18n.t("notice.updateSuccess")) }
     it { is_expected.to have_content("Updated Task 3") }
   end
 
@@ -53,11 +53,11 @@ RSpec.describe "Tasks", type: :system do
     let!(:task) { create(:task, name: "Task 4", status: :pending, priority: :medium) }
 
     before do
-      visit task_path(task)
-      click_button "Delete task"
+      visit task_path(I18n.locale, task)
+      click_button I18n.t("todo.deleteTask")
     end
 
-    it { is_expected.to have_content("Task was successfully deleted.") }
+    it { is_expected.to have_content(I18n.t("notice.deleteSuccess")) }
     it { is_expected.not_to have_content("Task 4") }
   end
 end
