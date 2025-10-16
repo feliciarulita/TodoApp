8e5cb6279175ba0fa48647e6c5132d04d2983aea
feliciarulita <feliciarulita@gmail.com>
2025-10-16 23:29:11 +0800

    add user index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_025832) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_16_152631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_025832) do
     t.boolean "manager", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users"
