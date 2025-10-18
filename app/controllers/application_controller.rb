a710bca4831c51d3585b67a57b35911afca5521e
feliciarulita <feliciarulita@gmail.com>
2025-10-18 10:22:03 +0800

    fix feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    return @current_user if defined?(@current_user)
+    return @current_user if @current_user
     @current_user = User.find_by(id: session[:user_id])
   end
 
